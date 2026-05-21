96da38030e19c939f605b8407b5a9b6526fb0944
prani <prani@DESKTOP-U6IQ5K7.home>
2026-05-21 16:35:03 -0400

    added from local

--- src/simple/SimpleClass.java
+++ src/simple/SimpleClass.java
@@ -1,13 +1,18 @@
 package simple;
 
+
+
 public class SimpleClass {
 
+	
+	
+	
+	
+    
+    
+  
 	public static void main(String[] args) {
-
-//i am adding this combine with remote so i can see both after merge
-
-		// changing it from remote
-
+		
 
 	}
 
